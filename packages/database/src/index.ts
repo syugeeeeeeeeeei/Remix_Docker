3b1cf01d1058c071a183d7f212b35dc6154dc462
syugeeeeeeeeeei <tomo.syugei99@gmail.com>
2025-07-04 16:37:47 +0900

    sadlkas;lfdsad;kljwqr

--- packages/database/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-// PrismaClientの型やEnumなどを再エクスポート
-export * from '@prisma/client';
-
-// PrismaClientのインスタンスを作成してデフォルトエクスポート
-const prisma = new PrismaClient();
-export default prisma;
